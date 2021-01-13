ca6ba355f7af23f66625c307e6ade7536937e0a3
Azeem Sweis <azeemsw@gmail.com>
2021-01-13 07:58:26 -0800

    Apply styles & refactor code

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const app = express();
 const port = 3000;
 
-let items = [];
+const items = [];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  let today = new Date();
-
-  let options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items
   });
 })
 
 app.post("/", (req,res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
-  items.push(item);
-  res.redirect('/');
+  if(req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+})
+
+app.get("/work", (req,res) => {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+})
+
+app.get("/about", (req, res) => {
+  res.render("about");
 })
 
 app.listen(port, function() {
